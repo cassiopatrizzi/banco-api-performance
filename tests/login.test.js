07230300a300c1541bf7f11792e94a0dd7ab585f
Cassio Patrizzi <cassio_patrizzi@yahoo.com>
2025-11-09 17:09:27 -0300

    Adicionando arquivo tests login

---
+++ tests/login.test.js
@@ -0,0 +1,33 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    iterations: 30,
+    thresholds: {
+        http_req_duration: ['p(90)<200', 'max<200'],
+        http_req_failed: ['rate<0.01'],
+    },
+};
+
+export default function () {
+    const url = 'http://localhost:3000/login';
+    const payload = JSON.stringify({
+        username: 'odila.vasquez',
+        senha: '079563',
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const response = http.post(url, payload, params);
+
+    check(response, { 
+        'Validar status = 200': (r) => r.status === 200,
+        'Validar token = string': (r) => typeof(r.json()).token === 'string'    
+    });
+
+    sleep(1);
+};
