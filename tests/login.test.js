19ff2ca555bfd5fe8aa251826d02c9f84316ccc5
Cassio Patrizzi <cassio_patrizzi@yahoo.com>
2025-11-09 18:19:42 -0300

    Adicionado fixtures para dados de entrada

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,20 +1,26 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
     stages: [
         { duration: '5s', target: 10 }, 
         { duration: '20s', target: 10 },  
         { duration: '5s', target: 0 },   
-    ]
+    ],
+    thesholds: {
+        http_req_duration: ['p(90)<3000', 'max<5000'], 
+        http_req_failed: ['rate<0.01']
+    }
 };
 
 export default function () {
     const url = 'http://localhost:3000/login';
-    const payload = JSON.stringify({
-        username: 'odila.vasquez',
-        senha: '079563',
-    });
+
+    postLogin.username = "jose.alcantara",
+    postLogin.senha = "502045"
+    
+    const payload = JSON.stringify(postLogin);
 
     const params = {
         headers: {
